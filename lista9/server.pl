35e336b78c510f4080d292b54780475d1bafd900
Szymon Wrobel <szywro5@gmail.com>
2018-06-02 02:07:57 +0200

    Use DCG rules for charcodes

--- lista9/server.pl
+++ lista9/server.pl
@@ -12,53 +12,52 @@ server(Port) :-
 	http_server(http_dispatch, [port(Port)]),
 	format('Queens: http://localhost:~w/queens~n', [Port]).
 
+start_html :-
+	format('Content-type: text/html~n~n'),
+	format('<!DOCTYPE html><html><head><title>Queens</title>~n'),
+	format('<meta http-equiv="content-type" content="text/html; charset=UTF-8">~n'),
+	format('</head><body>~n').
+
+end_html :- 
+	format('</body></html>').
+
 :- http_handler(root(queens), handler_queens, []).
 handler_queens(_) :- 
-		format('Content-type: text/html~n~n'),
-		format('<!DOCTYPE html><html><head><title>Queens</title>~n'),
-		format('<meta http-equiv="content-type" content="text/html; charset=UTF-8">~n'),
-		format('</head><body>~n'),
+		start_html,
 		format('<h1>Give problem size</h1>~n'),
 		build_form([action('solution'), method('post')], [
 			label(size, 'Size:'),
 			input(text, size),
 			input(submit, 'Submit')
-		]).
+		]),
+		end_html.
 
 :- http_handler(root(solution), handler_solution, []).
 handler_solution(Request) :- 
 	check_request(Request, N),
 	queens(N, Solution),
-	reply_html_page([], [
-		h1(['Solution for size ', N]),
-		table(
-			\format_solution(N, Solution)
-		)
-	]).
+	start_html,
+	format('<h1>Solution for N = ~w</h1>', [N]),
+	phrase(format_solution(N, Solution), TableBody),
+	format('<table>~s</table>', [TableBody]),
+	end_html.
 
 check_request(Request, N) :-
 	member(method(post), Request), !,
 	http_read_data(Request, Data, []),
-	member('size' = Size, Data),
+	member(size = Size, Data),
 	atom_number(Size, N).
 
 format_solution(_, []) --> [].
 format_solution(N, [H|T]) -->
-	html(
-		tr(
-			\format_row(N, H, 1)
-		)
-	),
+	`<tr>`, format_row(N, H, 1), `</tr>`,
 	format_solution(N, T).
 
-format_row(N, _, C) --> {C > N}, [].
-format_row(N, X, C) --> 
-	{ X =:= C -> Pic = 'queen.png' ; Pic = 'empty.png' }, 
-	html(
-		td(
-			img([src(Pic)])
-		)
-	),
-	{ C1 is C + 1 },
+format_row(N, _, C) --> {C > N}, ``.
+format_row(N, X, C) --> { X =:= C }, { C1 is C + 1 },
+	`<td><img src="queen.png"/></td>`,
 	format_row(N, X, C1). 
 
+format_row(N, X, C) --> { X =\= C }, { C1 is C + 1 },
+	`<td><img src="empty.png"/></td>`,
+	format_row(N, X, C1). 
