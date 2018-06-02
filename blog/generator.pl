f09702412bb0483c56e6eb164d84b809198635e7
Szymon Wrobel <szywro5@gmail.com>
2018-06-02 14:33:30 +0200

    END

--- blog/generator.pl
+++
@@ -1,5 +0,0 @@
-:- module(generator, [run/2]).
-
-:- use_module(library(pwp)).
-
-run(In, Out) :- pwp_files(In, Out).
--- structures/queue/trivial.pl
+++
@@ -1,9 +0,0 @@
-% Trivial queue
-% queue(S1, S2).
-
-dequeue(queue([H|T], L), H, queue(T, L)).
-dequeue(queue([], L), H, Rem) :-
-    reverse(Reverse, L),
-    dequeue(queue(Reverse, []), H, Rem).
-
-enqueue(queue(H, L), X, queue(H, [X|L])).
